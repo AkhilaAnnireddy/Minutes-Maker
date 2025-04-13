e268c4d3ef47be9a31dc902dd4e6de36db7ff47c
Akhila Annireddy <akhilaannireddy0605@gmail.com>
2025-04-13 11:47:04 -0600

    added terraform and github skeleton

---
+++ Infrasctructure/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.3.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "minute_maker" {
+  bucket = var.bucket_name
+
+  tags = {
+    Project = "MinuteMaker"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_object" "folders" {
+  for_each = toset(["models/", "input/", "intermediate/", "output/"])
+
+  bucket  = aws_s3_bucket.minute_maker.id
+  key     = each.key
+  content = "" # creates a 0-byte object directly
+}
+
+
+
---
+++ Infrasctructure/output.tf
@@ -0,0 +1,4 @@
+output "s3_bucket_name" {
+  description = "The name of the S3 bucket created"
+  value       = aws_s3_bucket.minute_maker.bucket
+}
