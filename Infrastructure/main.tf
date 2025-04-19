51d60beac3d9d1918f5b4e9699bbeeb7c67cbcb6
Akhila Annireddy <akhilaannireddy0605@gmail.com>
2025-04-19 16:00:43 -0600

    removed whisper related code as of now

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-# S3 Bucket for Minute Maker
+# S3 Bucket for Minute Maker (input bucket)
 resource "aws_s3_bucket" "minute_maker" {
   bucket = var.input_bucket_name
 
@@ -20,59 +20,4 @@ resource "aws_s3_bucket" "minute_maker" {
     Project     = "MinuteMaker"
     Environment = "Dev"
   }
-}
-
-
-# CloudWatch Log Group
-resource "aws_cloudwatch_log_group" "lambda_logs" {
-  name              = "/aws/lambda/whisper-lambda"
-  retention_in_days = 7
-}
-
-# IAM Role for Lambda
-resource "aws_iam_role" "lambda_exec_role" {
-  name = "whisper-lambda-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-# IAM Policy Attachment - Basic Lambda Execution
-resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-# IAM Policy - S3 Access for Lambda
-resource "aws_iam_role_policy" "lambda_s3_access" {
-  name = "whisper-lambda-s3-access"
-  role = aws_iam_role.lambda_exec_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:ListBucket"
-        ],
-        Resource = [
-          "arn:aws:s3:::${var.input_bucket_name}",
-          "arn:aws:s3:::${var.input_bucket_name}/*"
-        ]
-      }
-    ]
-  })
 }
