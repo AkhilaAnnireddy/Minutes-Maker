5c20dd37792c601c41a7d17cd63dd45b5b22b216
Akhila Annireddy <akhilaannireddy0605@gmail.com>
2025-04-19 21:05:20 -0600

    fixing typo errors

--- Infrastructure/api_gateway.tf
+++ Infrastructure/api_gateway.tf
@@ -12,7 +12,7 @@ resource "aws_apigatewayv2_integration" "video_upload_lambda_integration" {
 }
 
 resource "aws_apigatewayv2_route" "video_upload_route" {
-  api_id    = aws_apigatewayv2_api.video_upload_http_ap.i.id
+  api_id    = aws_apigatewayv2_api.video_upload_http_api.id
   route_key = "POST /upload"
   target    = "integrations/${aws_apigatewayv2_integration.video_upload_lambda_integration.id}"
 }
