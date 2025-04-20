56a23bebec59ea74213e73018a15a638d1f19c7b
Akhila Annireddy <akhilaannireddy0605@gmail.com>
2025-04-19 21:00:08 -0600

    fixed braces

--- Infrastructure/api_gateway.tf
+++ Infrastructure/api_gateway.tf
@@ -11,7 +11,7 @@ resource "aws_apigatewayv2_integration" "video_upload_lambda_integration" {
   payload_format_version = "2.0"
 }
 
-resource "aws_apigatewayv2_route" "video_upload_route" 
+resource "aws_apigatewayv2_route" "video_upload_route" {
   api_id    = aws_apigatewayv2_api.video_upload_http_ap.i.id
   route_key = "POST /upload"
   target    = "integrations/${aws_apigatewayv2_integration.video_upload_lambda_integration.id}"
